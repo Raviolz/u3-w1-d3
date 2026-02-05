d3bad7cef38b0fa6ebbabc5e7c6648fe4b6fb795
Raviolz <gi.ravioli@gmail.com>
2026-02-05 16:28:02 +0100

    fetch-get

--- src/components/CommentsArea.jsx
+++ src/components/CommentsArea.jsx
@@ -10,24 +10,56 @@
 //}
 //})
 
+const commentsURL = "https://striveschool-api.herokuapp.com/api/comments/"
+
 import { Component } from "react"
 import { ListGroup } from "react-bootstrap"
 
 class CommentsArea extends Component {
   state = { comments: [] }
+  getComments = () => {
+    fetch(commentsURL + this.props.asin, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczNDk0OTg1ZTNiMTAwMTViNWVlMTAiLCJpYXQiOjE3NzAzMDIyMTgsImV4cCI6MTc3MTUxMTgxOH0._ly49N0jk7aB-nHwhutGhQQOQVrO_j13U-Z5HNr4mj8",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("Errore nel recupero dei commenti")
+        }
+      })
+      .then((comment) => {
+        console.log("COMMENTS RECUPERATI", comment)
+
+        this.setState({
+          comments: comment,
+        })
+      })
+      .catch((err) => {
+        console.log("errore", err)
+      })
+  }
+  componentDidMount() {
+    this.getComments()
+  }
+
   render() {
     return (
       <>
-        {" "}
         <p className="text-muted mb-2">
           Commenti del libro: <strong>{this.props.asin}</strong>
         </p>
+
         <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          {this.state.comments.map((c) => (
+            <ListGroup.Item key={c._id}>
+              <div>{c.comment}</div>
+              <small className="text-muted">Voto: {c.rate}</small>
+            </ListGroup.Item>
+          ))}
         </ListGroup>
       </>
     )
